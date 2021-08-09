1261d42f9a054c354ea25496a4304124f1c6401d
Augus <bramlye@outlook.com>
2021-08-09 17:55:56 +0800

    ptr io 代码附加

---
+++ src/TRAINING/io.cc
@@ -0,0 +1,55 @@
+//
+// Created by AUGUS on 2021/8/9.
+//
+
+#include "io.hh"
+
+std::string binaryToHex(const std::string &binaryStr)
+{
+    std::string ret;
+    static const char *hex = "0123456789ABCDEF";
+    for (auto c:binaryStr) {
+        ret.push_back(hex[(c >> 4) & 0xf]); //取二进制高四位
+        ret.push_back(hex[c & 0xf]);        //取二进制低四位
+    }
+    return ret;
+}
+
+void io_test::string2binary()
+{
+    std::string sss =
+        R"({"Account": "","FortressUser": "","AppName": "","AppPath": "","AppHash": "","Mac": "","Token": "","dport": 0,"TerminalPort": 0,"dst": "","TerminalIP": "","dbid": 0})";
+
+    const char *json =
+        R"({"Account": "","FortressUser": "","AppName": "","AppPath": "","AppHash": "","Mac": "","Token": "","dport": 0,"TerminalPort": 0,"dst": "","TerminalIP": "","dbid": 0})";
+    std::cout << strlen(json) << std::endl;
+
+    char a[164];
+    strcpy(a, json);
+
+    for (auto ss:a) {
+        std::cout << ss;
+    }
+
+    std::string changed = binaryToHex(a);
+
+    std::cout << "\n string ssssssssss->";
+    for (auto ss:changed) {
+        std::cout << ss;
+    }
+    std::cout << "\n";
+
+    //注意修改工作路径 为 yourpath\AugusTest\src\COMPANY_TEST
+    std::fstream f;
+    std::ifstream fin("log/data.txt");   //读取文件
+    if (!fin) {
+        std::cerr << "fail" << std::endl;
+    }
+    //追加写入,在原来基础上加了ios::app
+    f.open("log/data.txt", std::ios::out | std::ios::app);
+    //输入你想写入的内容
+    f << "\n\n" << changed << std::endl;
+    f.close();
+
+}
+
