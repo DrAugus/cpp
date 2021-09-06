aea6206f147b4847991569dce9ac4c81f107b4d7
Augus <bramlye@outlook.com>
2021-09-07 00:02:07 +0800

    移除bug文件

--- src/HELP_OTHERS/main.cc
+++ src/HELP_OTHERS/main.cc
@@ -2,15 +2,10 @@
 // Created by AUGUS on 2021/5/31.
 //
 
-#include "store_manage.hh"
 
 #include "gtest/gtest.h"
 
-int main(int argc, char *argv[])
-{
-
-//    testp();
-    store_manage();
+int main(int argc, char *argv[]) {
 
     testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
