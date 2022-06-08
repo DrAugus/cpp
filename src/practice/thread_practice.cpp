a7f477d39d0f8c9b8d7f7322c662331cd759a3fe
Augus <bramlye@outlook.com>
2022-06-08 11:33:15 +0800

    refactor: 调整 thread_practice.cpp 结构
    当前仅在win可正常编译，linux无法通过编译

--- src/practice/thread_practice.cpp
+++ src/practice/thread_practice.cpp
@@ -4,9 +4,9 @@
 
 #include "thread_practice.hh"
 
+std::thread::id main_thread_id = std::this_thread::get_id();
 
-void thread_practice::is_main_thread()
-{
+void is_main_thread() {
     if (main_thread_id == std::this_thread::get_id()) {
         std::cout << "This is the main thread.\n";
     } else {
@@ -14,11 +14,10 @@ void thread_practice::is_main_thread()
     }
 }
 
-void thread_practice::thread_test()
-{
-    //template <class Rep, class Period>
-    //       void sleep_for (const chrono::duration<Rep,Period>& rel_time);
-    //阻塞调用线程，一直到指定时间段后。
+void thread_test() {
+    // template <class Rep, class Period>
+    //        void sleep_for (const chrono::duration<Rep,Period>& rel_time);
+    // 阻塞调用线程，一直到指定时间段后。
     std::cout << "countdown:\n";
     for (int i = 10; i > 0; --i) {
         std::cout << i << '\n';
@@ -29,9 +28,9 @@ void thread_practice::thread_test()
     is_main_thread();
     std::cout << std::this_thread::get_id() << std::endl;
 
-    //template <class Clock, class Duration>
-    //       void sleep_until (const chrono::time_point<Clock,Duration>& abs_time);
-    //阻塞调用线程，一直到指定事件
+    // template <class Clock, class Duration>
+    //        void sleep_until (const chrono::time_point<Clock,Duration>& abs_time);
+    // 阻塞调用线程，一直到指定事件
     using std::chrono::system_clock;
     std::time_t tt = system_clock::to_time_t(system_clock::now());
 
@@ -47,39 +46,36 @@ void thread_practice::thread_test()
     std::cout << std::this_thread::get_id() << std::endl;
 }
 
-
-int test_thread::threadStart(const std::string &tName)
-{
+int threadStart(const std::string &tName) {
     std::cout << "func[threadStart]###" << tName << std::endl;
     return 0;
 }
 
-int test_thread::threadInt(int a)
-{
+int threadInt(int a) {
     std::cout << "func[threadInt]### - [" << a << "]\n";
     return 0;
 }
 
-int test_thread::testThread()
-{
+int testThread() {
     std::cout << "\nBELLOW ARE THREAD TEST -------\n\n";
 
-    std::thread t1(test_thread::threadStart, " c++ 11 thread");
+    std::thread t1(threadStart, " c++ 11 thread");
     int arg = 0;
     std::thread t2;
-    std::thread t3(test_thread::threadInt, arg + 1);
-    std::thread t4(test_thread::threadInt, std::ref(arg));
+    std::thread t3(threadInt, arg + 1);
+    std::thread t4(threadInt, std::ref(arg));
     std::thread t5(std::move(t4));
 
     auto judgeJoin = [&](std::thread &t, const std::string &tName) {
-      std::cout << "Thread ID " << t.get_id() << "-" << std::this_thread::get_id() << std::endl;
-      if (t.joinable()) {
-          std::cout << tName << " can join";
-          t.join();
-          std::cout << std::endl;
-      } else {
-          std::cout << tName << " can not join\n";
-      }
+        std::cout << "Thread ID " << t.get_id() << "-" << std::this_thread::get_id()
+                  << std::endl;
+        if (t.joinable()) {
+            std::cout << tName << " can join";
+            t.join();
+            std::cout << std::endl;
+        } else {
+            std::cout << tName << " can not join\n";
+        }
     };
 
     judgeJoin(t1, "t1");
@@ -88,78 +84,96 @@ int test_thread::testThread()
     judgeJoin(t4, "t4");
     judgeJoin(t5, "t5");
 
-    std::thread t6(test_thread::threadStart, "C++ 11 thread_1!");
-    std::thread t7(test_thread::threadStart, "C++ 11 thread_2!");
+    std::thread t6(threadStart, "C++ 11 thread_1!");
+    std::thread t7(threadStart, "C++ 11 thread_2!");
     std::cout << "current thread id: " << std::this_thread::get_id() << std::endl;
-    std::cout << "before swap: " << " thread_1 id: " << t6.get_id() << " thread_2 id: " << t7.get_id() << std::endl;
+    std::cout << "before swap: "
+              << " thread_1 id: " << t6.get_id() << " thread_2 id: " << t7.get_id()
+              << std::endl;
     t6.swap(t7);
-    std::cout << "after swap: " << " thread_1 id: " << t6.get_id() << " thread_2 id: " << t7.get_id() << std::endl;
-    //t.detach();
+    std::cout << "after swap: "
+              << " thread_1 id: " << t6.get_id() << " thread_2 id: " << t7.get_id()
+              << std::endl;
+    // t.detach();
     t6.join();
     t7.join();
-
 
     return 0;
 }
 
-namespace thread_practice {
-
 void PaoA(char const *name) { std::cout << "和" << name << "看文艺电影" << std::endl; }
 
-void PaoB(char const *name) { std::cout << "陪" << name << "逛街购物，我花钱" << std::endl; }
+void PaoB(char const *name) {
+    std::cout << "陪" << name << "逛街购物，我花钱" << std::endl;
+}
 
-void PaoC(char const *name) { std::cout << name << "，走，到军博看大炮去！" << std::endl; }
+void PaoC(char const *name) {
+    std::cout << name << "，走，到军博看大炮去！" << std::endl;
+}
 
 void PaoD(char const *name) { std::cout << name << "，我们来谈人生好吗？" << std::endl; }
 
 void PaoE(char const *name) { std::cout << name << "，川菜还是撸串？" << std::endl; }
 
-char const *names[] = {
-    "丁晓晓", "贾玲玲", "林子绿", "王梦", "水原希"
-};
+char const *names[] = {"丁晓晓", "贾玲玲", "林子绿", "王梦", "水原希"};
 
 using F = void (*)(char const *);
 
-F functions[] =
-    {
-        PaoA, PaoB, PaoC, PaoD, PaoE
-    };
+F functions[] = {PaoA, PaoB, PaoC, PaoD, PaoE};
 
 constexpr size_t N = sizeof(names) / sizeof(names[0]);
 
-void WeekendDate()
-{
+void WeekendDate() {
     for (auto i = 0; i < N; ++i) {
         functions[i](names[i]);
     }
 }
 
-void CrazyWeekendDate()
-{
+void CrazyWeekendDate() {
     std::thread trds[N];
     for (size_t i = 0; i < N; ++i) {
-        auto trd = std::thread([i] {
-          functions[i](names[i]);
-        });
+        auto trd = std::thread([i] { functions[i](names[i]); });
         trds[i] = move(trd);
     }
 
-    for (auto &trd: trds) {
+    for (auto &trd : trds) {
         trd.join();
     }
 }
 
+void threadTask() { std::cout << "this is my thread task" << std::endl; }
 
-void threadTask()
-{
-    std::cout << "this is my thread task" << std::endl;
+std::atomic<bool> ready(false);
+
+void count1m(int id) {
+    while (!ready) {  // wait until main() sets ready...
+        std::this_thread::yield();
+    }
+    std::cout << id;
 }
 
-}//namespace thread_practice
+int thread_main() {
+    std::thread threads[10];
+    std::cout << "race of 10 threads that count to 1 million:\n";
+    for (int i = 0; i < 10; ++i) {
+        threads[i] = std::thread(count1m, i);
+    }
+    ready = true;  // go!
+    for (auto &th : threads) {
+        th.join();
+    }
+    std::cout << '\n';
 
+    return 0;
+}
 
-TEST(thread_practice, threadTask)
-{
-    std::thread t(thread_practice::threadTask);
+int main() {
+    std::thread t(threadTask);
     t.join();
+
+    thread_test();
+    testThread();
+    thread_main();
+
+    return 0;
 }
--- src/practice/thread_practice.hh
+++ src/practice/thread_practice.hh
@@ -5,49 +5,6 @@
 #ifndef AUGUSTEST_THREAD_PRACTICE_HH
 #define AUGUSTEST_THREAD_PRACTICE_HH
 
-
 #include "augus/augus.h"
 
-namespace thread_practice {
-std::thread::id main_thread_id = std::this_thread::get_id();
-
-void is_main_thread();
-void thread_test();
-
-}//namespace thread_practice
-
-namespace test_thread {
-
-int threadStart(const std::string &tName);
-int threadInt(int a);
-int testThread();
-
-}//namespace test_thread
-namespace example_yield {
-std::atomic<bool> ready(false);
-
-void count1m(int id)
-{
-    while (!ready) { // wait until main() sets ready...
-        std::this_thread::yield();
-    }
-    for (volatile int i = 0; i < 1000000; ++i) {}
-    std::cout << id;
-}
-
-int thread_main()
-{
-    std::thread threads[10];
-    std::cout << "race of 10 threads that count to 1 million:\n";
-    for (int i = 0; i < 10; ++i) { threads[i] = std::thread(count1m, i); }
-    ready = true; // go!
-    for (auto &th : threads) { th.join(); }
-    std::cout << '\n';
-
-    return 0;
-}
-
-}//namespace example_yield
-
-
-#endif //AUGUSTEST_THREAD_PRACTICE_HH
+#endif  // AUGUSTEST_THREAD_PRACTICE_HH
