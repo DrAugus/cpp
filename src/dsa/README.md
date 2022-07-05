89e2138582cb63606e0c3cc804d1e8e1231b646d
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-07-05 18:09:20 +0800

    feat: dsa

---
+++ src/dsa/README.md
@@ -0,0 +1,91 @@
+# DSA
+
+- **Search** − Algorithm to search an item in a data structure.
+- **Sort** − Algorithm to sort items in a certain order.
+- **Insert** − Algorithm to insert item in a data structure.
+- **Update** −  Algorithm to update an existing item in a data structure.
+- **Delete** − Algorithm to delete an existing item from a data structure.
+
+## Greedy Algorithms
+
+- Travelling Salesman Problem 
+- Prim's Minimal Spanning Tree Algorithm
+- Kruskal's Minimal Spanning Tree Algorithm
+- Dijkstra's Minimal Spanning Tree Algorithm
+- Graph - Map Coloring
+- Graph - Vertex Cover
+- Knapsack Problem
+- Job Scheduling Problem
+
+## Divide and Conquer
+
+- Merge Sort
+- Quick Sort
+- Binary Search
+- Strassen's Matrix Multiplication
+- Closest pair (points)
+
+Broadly, we can understand divide-and-conquer approach in a three-step process.
+
+**Divide/Break**
+
+This step involves breaking the problem into smaller sub-problems. Sub-problems should represent a part of the original problem. This step generally takes a recursive approach to divide the problem until no sub-problem is further divisible. At this stage, sub-problems become atomic in nature but still represent some part of the actual problem.
+
+**Conquer/Solve**
+
+This step receives a lot of smaller sub-problems to be solved. Generally, at this level, the problems are considered 'solved' on their own.
+
+**Merge/Combine**
+
+When the smaller sub-problems are solved, this stage recursively combines them until they formulate a solution of the original problem. This algorithmic approach works recursively and conquer & merge steps works so close that they appear as one.
+
+## Dynamic Programming
+
+- Fibonacci number series
+- Knapsack problem
+- Tower of Hanoi
+- All pair shortest path by Floyd-Warshall
+- Shortest path by Dijkstra
+- Project scheduling
+
+## DSA Basic Concepts
+
+### Data Object
+
+**Data Object** represents an object having a data.
+
+### Data type
+
+**Data type** is a way to classify various types of data such as integer, string, etc. which determines the values that can be used with the corresponding type of data, the type of operations that can be performed on the corresponding type of data. There are two data types
+- [Built-in Data Type](#Built-in Data Type)
+- [Derived Data Type](#(#Built-in Data Type))
+
+### Built-in Data Type
+
+- Integers
+- Boolean (true, false)
+- Floating (Decimal numbers)
+- Character and Strings
+
+### Derived Data Type
+
+- List
+- Array
+- Stack
+- Queue
+
+### Basic Operations
+
+- Traversing
+- Searching
+- Insertion
+- Deletion
+- Sorting
+- Merging
+
+## [Linked List](./linked_list.cpp)
+
+- **Simple Linked List** − Item navigation is forward only.
+- **Doubly Linked List** − Items can be navigated forward and backward.
+- **Circular Linked List** − Last item contains link of the primo element as next and the first element has a link to the last element as previous.
+
---
+++ src/dsa/linked_list.cpp
@@ -0,0 +1,151 @@
+//
+// linked_list.cpp
+// Created by AUGUS on 2022/7/5.
+//
+
+#include <iostream>
+#include <random>
+
+struct LinkedListNode {
+    int key;
+    int value;
+    LinkedListNode *next;
+};
+
+//Insertion − Adds an element at the beginning of the list.
+//Deletion − Deletes an element at the beginning of the list.
+//Insert Last − Adds an element at the end of the list.
+//Delete Last − Deletes an element from the end of the list.
+//Insert After − Adds an element after an item of the list.
+//Delete − Deletes an element from the list using the key.
+//Display forward − Displays the complete list in a forward manner.
+//Display backward − Displays the complete list in a backward manner.
+struct DoublyLinkedListNode : LinkedListNode {
+    int key;
+    int value;
+    DoublyLinkedListNode *next;
+    DoublyLinkedListNode *prev;
+};
+
+struct DoublyLinkedListNode *head = nullptr;
+struct DoublyLinkedListNode *tail = nullptr;
+
+bool isEmpty() {
+    return head == nullptr;
+}
+
+int length() {
+    int length = 0;
+    while (head->next) {
+        ++length;
+    }
+    return length;
+}
+
+void displayForward() {
+    auto *p = head;
+    std::cout << "[";
+    while (p) {
+        std::cout << "(" << p->key << "," << p->value << ")";
+        p = p->next;
+    }
+    std::cout << "]\n";
+}
+
+void displayBackward() {
+    auto *p = tail;
+    std::cout << "[";
+    while (p) {
+        std::cout << "(" << p->key << "," << p->value << ")";
+        p = p->prev;
+    }
+    std::cout << "]\n";
+}
+
+DoublyLinkedListNode *create(int len) {
+    if (len <= 0) {
+        std::cerr << "wrong length" << std::endl;
+    }
+    std::random_device rd;
+    head = nullptr;
+    tail = nullptr;
+
+    int key = 0;
+    while (--len) {
+        auto *link = new DoublyLinkedListNode;
+        link->key = ++key;
+        link->value = (int) rd();
+
+        if (key == 1) {
+            tail = link;
+            link->next = head;
+            head = link;
+            continue;
+        }
+
+        auto *current = head;
+        if (current == tail) {
+            link->next = nullptr;
+            tail = link;
+        } else {
+            link->next = current->next;
+            current->next->prev = link;
+        }
+        link->prev = current;
+        current->next = link;
+
+    }
+    return head;
+}
+
+bool insert(int insertPos, int key, int value) {
+    auto *link = new DoublyLinkedListNode;
+    link->key = key;
+    link->value = value;
+
+    if (insertPos == 0) {
+        // insert head
+        if (isEmpty()) {
+            tail = link;
+        } else {
+            head->prev = link;
+        }
+        link->next = head;
+        head = link;
+    } else if (insertPos == length()) {
+        // insert tail
+        if (isEmpty()) {
+            tail = link;
+        } else {
+            tail->next = link;
+            link->prev = tail;
+        }
+        tail = link;
+    } else {
+        if (isEmpty()) return false;
+        auto *current = head;
+        while (current->key != key) {
+            if (current->next) {
+                current = current->next;
+                continue;
+            }
+            return false;
+        }
+        if (current == tail) {
+            link->next = nullptr;
+            tail = link;
+        } else {
+            link->next = current->next;
+            current->next->prev = link;
+        }
+        link->prev = current;
+        current->next = link;
+    }
+    return true;
+}
+
+
+int main() {
+//    displayForward();
+    return 0;
+}
