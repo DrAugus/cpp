093927a5bcd8cf19677e8691f8a512113a037d3e
Augus <bramlye@outlook.com>
2022-07-30 01:20:31 +0800

    :sparkles: feat: leetcode 700

---
+++ src/leetcode/700.cpp
@@ -0,0 +1,18 @@
+//
+// Created by braml on 2022/7/30.
+// 二叉搜索树中的搜索
+//
+
+#include "utils.h"
+
+namespace leetcode {
+    TreeNode *searchBST(TreeNode *root, int val) {
+        if (root == nullptr || root->value == val) return root;
+        if (root->value > val) return searchBST(root->left, val);
+        else return searchBST(root->right, val);
+    }
+}
+
+int main() {
+    return 1;
+}
