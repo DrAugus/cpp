5ffaf5867d2f428700e80f353ce580bc863e8a8c
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-08-18 01:05:02 +0800

    :sparkles: feat: Attribute Parser

--- src/practice/attribute_parser.cpp
+++ src/practice/attribute_parser.cpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <algorithm>
 #include <vector>
+#include <unordered_map>
 
 void remove_str_space(std::string &str, char remove) {
     str.erase(std::remove(str.begin(), str.end(), remove), str.end());
@@ -27,46 +28,122 @@ std::array<std::string, 2> get_k_v(const std::string &str) {
     return {l, r};
 }
 
+int get_num_from_str(const std::string &str) {
+    std::string res;
+    for (auto s: str) {
+        if (s >= '0' && s <= '9') {
+            res += s;
+        }
+    }
+    if (res.empty())
+        return 0;
+    return std::stoi(res);
+}
+
 int main() {
 
     int N, Q;
     std::cin >> N >> Q;
-    std::vector<std::string> tag;
-    for (int i = 1; i <= N / 2; ++i) {
-        tag.push_back("tag" + std::to_string(i));
-    }
-    std::vector<std::array<std::string, 2>> val;
+
+    using tag_info = std::vector<std::array<std::string, 2>>;
+    std::vector<std::unordered_map<std::string, tag_info> > val;
+
+
     for (int i = 0; i < N; ++i) {
         // record first half part
-        std::string tmps;
-        std::cin.get();
-        getline(std::cin, tmps);
+        std::string tmp_str;
+        // 因上方读取了 int cin 用 get() 获取前面空白
+        if (i == 0) std::cin.get();
+        getline(std::cin, tmp_str);
 
         if (i < N / 2) {
-            std::string remain;
-            // remove <tagx and >
-            if (N / 2 > 9) remain = tmps.substr(7, tmps.size() - 8);
-            else remain = tmps.substr(6, tmps.size() - 7);
+            std::string remain, tag_str;
+            // remove <tagx and > 保留空格不然会多裁剪
+            if (N / 2 > 9) {
+                remain = tmp_str.substr(6, tmp_str.size() - 7);
+                tag_str = tmp_str.substr(1, 5);
+
+            } else {
+                remain = tmp_str.substr(5, tmp_str.size() - 6);
+                tag_str = tmp_str.substr(1, 4);
+            }
 
             std::vector<std::string> vstemp;
-            std::string catchone;
+            std::string catch_one;
             for (auto ch: remain) {
-                catchone += ch;
-                bool judge = count(catchone.begin(), catchone.end(), '"') == 2 &&
-                             count(catchone.begin(), catchone.end(), '=') == 1;
+                catch_one += ch;
+                bool judge = count(catch_one.begin(), catch_one.end(), '"') == 2 &&
+                             count(catch_one.begin(), catch_one.end(), '=') == 1;
                 if (judge) {
-                    vstemp.push_back(catchone);
-                    catchone = "";
+                    vstemp.push_back(catch_one);
+                    catch_one = "";
                 }
             }
+
+            tag_info ti;
 
             for (auto &iii: vstemp) {
                 remove_str_space(iii, ' ');
                 remove_str_space(iii, '"');
-                val.push_back(get_k_v(iii));
+                ti.push_back(get_k_v(iii));
             }
+
+            std::unordered_map<std::string, tag_info> single_info;
+            single_info[tag_str] = ti;
+
+            val.push_back(single_info);
+
         }
 
     }
+
+
+    std::cout << "\n======= catch key value =======\n";
+
+    for (const auto &info: val) {
+        for (const auto &tt: info) {
+            std::cout << tt.first << " -> ";
+            for (auto kv: tt.second) {
+                std::cout << kv[0] << ":" << kv[1] << " ";
+            }
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\n======= catch end =======\n";
+
+
+    for (int i = 0; i < Q; ++i) {
+        std::string tmp_str;
+        getline(std::cin, tmp_str);
+
+        std::string catch_one;
+        std::vector<std::string> vec_catch;
+        for (auto ch: tmp_str) {
+            if (ch == '.' || ch == '~') {
+                vec_catch.push_back(catch_one);
+                catch_one = "";
+                continue;
+            }
+            catch_one += ch;
+        }
+        std::string value_str = tmp_str.substr(tmp_str.find('~') + 1);
+
+
+        std::cout << "\n==------==\n";
+
+
+        for (const auto &v: vec_catch) {
+            std::cout << get_num_from_str(v) << "  ";
+
+            std::cout << v << "  ";
+        }
+        std::cout << "\n";
+        std::cout << value_str << "   ===";
+
+    }
+
+    std::cout << "\n===end===\n";
+
     return 0;
 }
