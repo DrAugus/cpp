5818dc73563493eece6c20657a4d35f4c75f6e5b
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-09-25 13:02:14 +0800

    add 3rd fmt

--- src/primer/primer_test.cpp
+++ src/primer/primer_test.cpp
@@ -3,8 +3,21 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <3rd/fmt/include/fmt/core.h>
+
+
+int cal_sum(int x) {
+    if (!x) return 0;
+    return x + cal_sum(x - 1);
+}
 
 int main() {
+    fmt::print("cal sum {}\n", cal_sum(5));
+
+    std::cout << "take it "
+                 "easy?" << std::endl;
+
     int sum = 0, val = 50;
     while (val <= 100) {
         sum += val;
